e9166ebc4126dc0c3ddda6824582e63b46b99de4
Maxwell Duarte <maxmelodia@gmail.com>
2021-06-22 23:24:24 -0300

    feat: :sparkles: Adicionado "Contextos" e autenticação
    Transitando usuário logado com "contextos" do react

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,25 @@
 import { useHistory } from 'react-router-dom'
 
-import { auth, firebase } from '../services/firebase';
-
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
 import { Button } from '../components/Button'
+import { useAuth } from '../hooks/useAuth';
 
 import '../styles/auth.scss';
 
 export function Home() {
   const history = useHistory();
 
-  function handleCreateRoom(){
-    const provider = new firebase.auth.GoogleAuthProvider();
+  const { user, signInWithGoogle } = useAuth();
 
-    auth.signInWithPopup(provider).then(result => {
-      console.log(result);
+  async function handleCreateRoom(){
+    if (!user) {
+      await signInWithGoogle();
+    }
 
-      history.push('/rooms/new');  
-    })  
+    history.push('/rooms/new');  
   }
 
   return (
